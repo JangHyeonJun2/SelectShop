d5b6a0ad66dae8567c9c9547e59ff094dd822723
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-19 22:04:56 +0900

    PutMapping & DeleteMapping 구현하기

--- src/main/java/com/sparta/week01/Week01Application.java
+++ src/main/java/com/sparta/week01/Week01Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -48,6 +49,7 @@ public class Week01Application {
                 System.out.println(course.getTutor());
             }
         };
+
     }
 
 }
--- src/main/java/com/sparta/week01/controller/CourseController.java
+++ src/main/java/com/sparta/week01/controller/CourseController.java
@@ -5,10 +5,7 @@ import com.sparta.week01.domain.CourseRepository;
 import com.sparta.week01.models.CourseRequestDto;
 import com.sparta.week01.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +35,16 @@ public class CourseController {
 
         // JPA를 이용하여 DB에 저장하고, 그 결과를 반환합니다.
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/api/courses/{id}")
+    public Long updateCourse(@PathVariable Long id, @RequestBody CourseRequestDto requestDto) {
+        return courseService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/courses/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseRepository.deleteById(id);
+
     }
 }
