a0e65c0902006ba976c71204e4b3866be157e815
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-19 21:15:04 +0900

    생성일자, 수정일자를 자동으로 해주는 Timestamped클래스 추가 및 JPA Update 사용해보기

--- src/main/java/com/sparta/week01/domain/Course.java
+++ src/main/java/com/sparta/week01/domain/Course.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @NoArgsConstructor // 기본생성자를 대신 생성해줍니다.
 @Entity // 테이블임을 나타냅니다.
-public class Course {
+public class Course extends Timestamped{
 
     @Id // ID 값, Primary Key로 사용하겠다는 뜻입니다.
     @GeneratedValue(strategy = GenerationType.AUTO) // 자동 증가 명령입니다.
@@ -26,8 +26,17 @@ public class Course {
         return this.tutor;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public Course(String title, String tutor) {
         this.title = title;
         this.tutor = tutor;
+    }
+
+    public void update(Course course) {
+        this.title = course.title;
+        this.tutor = course.tutor;
     }
 }
