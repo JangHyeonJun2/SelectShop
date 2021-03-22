7fa4a9bbde303327c55dd7e9db33eb2683fdd98f
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-22 12:46:53 +0900

    네이버 API를 사용해서 검색한 상품들을 받고 이 상품정보를 JSON형태로 바꾼뒤 ItemDto객체에 넣고 List로 return 구현하기

---
+++ src/main/java/com/sparta/week01/models/ItemDto.java
@@ -0,0 +1,20 @@
+package com.sparta.week01.models;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ItemDto {
+    //title, link, image, lprice
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.link = itemJson.getString("link");
+        this.image = itemJson.getString("image");
+        this.lprice = itemJson.getInt("lprice");
+    }
+}
--- src/main/java/com/sparta/week01/utils/NaverShopSearch.java
+++ src/main/java/com/sparta/week01/utils/NaverShopSearch.java
@@ -1,10 +1,18 @@
 package com.sparta.week01.utils;
 
+import com.sparta.week01.domain.Product;
+import com.sparta.week01.models.ItemDto;
+import com.sun.tools.javac.jvm.Items;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NaverShopSearch {
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "d5iSJObOSR_ohpmrDzdY");
@@ -12,18 +20,28 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=adidas", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
         System.out.println("Response status: " + status);
-        System.out.println(response);
+//        System.out.println(response);
 
         return response;
     }
 
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+    public List<ItemDto> fromJsontoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
     }
 }
