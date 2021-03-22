ac9c10139e9bcf80315a88b9a20514a9fd6fc769
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-22 11:53:34 +0900

    Product 도메인,레포지토리,컨트롤러 생성하기

--- src/main/java/com/sparta/week01/Week01Application.java
+++ src/main/java/com/sparta/week01/Week01Application.java
@@ -24,32 +24,32 @@ public class Week01Application {
 
     }
 
-    @Bean
-    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
-        return (args) -> {
-            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
-
-            System.out.println("데이터 인쇄");
-            List<Course> courseList = courseRepository.findAll();
-            for (int i=0; i<courseList.size(); i++) {
-                Course course = courseList.get(i);
-                System.out.println(course.getId());
-                System.out.println(course.getTitle());
-                System.out.println(course.getTutor());
-            }
-
-//            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
-            CourseRequestDto new_course = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
-            courseService.update(1L, new_course);
-            courseList = courseRepository.findAll();
-            for (int i=0; i<courseList.size(); i++) {
-                Course course = courseList.get(i);
-                System.out.println(course.getId());
-                System.out.println(course.getTitle());
-                System.out.println(course.getTutor());
-            }
-        };
-
-    }
+//    @Bean
+//    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+//        return (args) -> {
+//            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+//
+//            System.out.println("데이터 인쇄");
+//            List<Course> courseList = courseRepository.findAll();
+//            for (int i=0; i<courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//
+////            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
+//            CourseRequestDto new_course = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+//            courseService.update(1L, new_course);
+//            courseList = courseRepository.findAll();
+//            for (int i=0; i<courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//        };
+//
+//    }
 
 }
